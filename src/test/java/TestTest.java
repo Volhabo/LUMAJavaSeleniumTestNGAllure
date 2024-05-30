20673e29415c0bba62b53f93b8d8b1566c0d0bd4
Volhabo <99760694+Volhabo@users.noreply.github.com>
2024-05-30 20:59:04 +0200

    add allure properties (#5)
    add allure properties

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,8 +1,10 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
     @Test
-    public void test(){
+    public void testPass(){
 
+        Assert.assertTrue(true);
     }
 }
