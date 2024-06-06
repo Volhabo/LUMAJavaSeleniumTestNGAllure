a65f37cb592163a8b1bbf301ac82781d8dfae09f
Volhabo <barouskayavolha@gmail.com>
2024-06-06 19:42:50 +0200

    fix DriverUtils

--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -2,17 +2,47 @@ package com.lumatest.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Map;
+
 public class DriverUtils {
+    private static ChromeOptions chromeOptions;
+
+    static {
+        chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-web-security");
+        chromeOptions.addArguments("--allow-running-insecure-content");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+    }
+
     public static WebDriver createChromeDriver(WebDriver driver) {
-        if (driver == null) {
-            return new ChromeDriver();
-        } else {
+        if (driver != null) {
             driver.quit();
-            return new ChromeDriver();
+        }
+        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+        chromeDriver.executeCdpCommand("Network.enable", Map.of());
+        chromeDriver.executeCdpCommand("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
+
+        return chromeDriver;
         }
     }
+//    public static WebDriver createChromeDriver(WebDriver driver) {
+//        if (driver == null) {
+//            return new ChromeDriver(chromeOptions);
+//        } else {
+//            driver.quit();
+//            return new ChromeDriver(chromeOptions);
+//        }
+//    }
 
 //    private WebDriver createFireFoxDriver(WebDriver driver) {
 //        if (driver == null) {
@@ -24,4 +54,4 @@ public class DriverUtils {
 //
 //        return driver;
 //    }
-}
+//}
