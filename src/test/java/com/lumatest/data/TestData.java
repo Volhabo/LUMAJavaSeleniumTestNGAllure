a53abb926e830cd2dddea6ae69c917987f51ae23
Volhabo <barouskayavolha@gmail.com>
2024-06-06 18:05:47 +0200

    update NavigationTest,TestData

--- src/test/java/com/lumatest/data/TestData.java
+++ src/test/java/com/lumatest/data/TestData.java
@@ -1,6 +1,46 @@
 package com.lumatest.data;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
+
 public class TestData {
     public static final String BASE_URL = "https://magento.softwaretestingboard.com";
     public static final String BASE_URL_TITLE = "Home Page";
+
+    public static final By WHATS_NEW_MENU = By.xpath("//nav//span[text()=\"What's New\"]");
+    public static final String WHATS_NEW_URL = BASE_URL + "/what-is-new.html";
+    public static final String WHATS_NEW_TITLE = "What's New";
+
+    public static final By WOMEN_MENU = By.xpath("//nav//span[text()=\"Women\"]");
+    public static final String WOMEN_URL = BASE_URL + "/women.html";
+    public static final String WOMEN_TITLE = "Women";
+
+    public static final By MEN_MENU = By.xpath("//nav//span[text()=\"Men\"]");
+    public static final String MEN_URL = BASE_URL + "/men.html";
+    public static final String MEN_TITLE = "Men";
+
+    public static final By GEAR_MENU = By.xpath("//nav//span[text()=\"Gear\"]");
+    public static final String GEAR_URL = BASE_URL + "/gear.html";
+    public static final String GEAR_TITLE = "Gear";
+
+    public static final By TRAINING_MENU = By.xpath("//nav//span[text()=\"Training\"]");
+    public static final String TRAINING_URL = BASE_URL + "/training.html";
+    public static final String TRAINING_TITLE = "Training";
+
+    public static final By SALE_MENU = By.xpath("//nav//span[text()='Sale']");
+    public static final String SALE_URL = BASE_URL + "/sale.html";
+    public static final String SALE_TITLE = "Sale";
+
+    @DataProvider(name = "navigationData")
+    public static final Object[][] getNavMenuData() {
+        return new Object[][]{
+                {BASE_URL, WHATS_NEW_MENU, WHATS_NEW_URL, WHATS_NEW_TITLE},
+                {BASE_URL, WOMEN_MENU, WOMEN_URL, WOMEN_TITLE},
+                {BASE_URL, MEN_MENU, MEN_URL, MEN_TITLE},
+                {BASE_URL, GEAR_MENU, GEAR_URL, GEAR_TITLE},
+                {BASE_URL, TRAINING_MENU, TRAINING_URL, TRAINING_TITLE},
+                {BASE_URL, SALE_MENU, SALE_URL, SALE_TITLE}
+        };
+    }
 }
+
--- src/test/java/com/lumatest/test/NavigationTest.java
+++ src/test/java/com/lumatest/test/NavigationTest.java
@@ -3,6 +3,7 @@ package com.lumatest.test;
 import com.lumatest.base.BaseTest;
 import com.lumatest.data.TestData;
 import io.qameta.allure.*;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,6 +21,25 @@ public class NavigationTest extends BaseTest {
 
         Allure.step("Open BaseURL");
         getDriver().get(TestData.BASE_URL);
+
+        Allure.step("Collect actualURL, actualTitle");
+        final String actualURL = getDriver().getCurrentUrl();
+        final String actualTitle = getDriver().getTitle();
+
+        Allure.step("Verify actualURL as expected");
+        Assert.assertEquals(actualURL, expectedURL);
+        Allure.step("Verify actualTitle as expected");
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test(dataProvider = "navigationData", dataProviderClass = TestData.class)
+    public void testNavigationMenu(String baseURL, By navbarMenu, String expectedURL, String expectedTitle) {
+
+        Allure.step("Open Base URL");
+        getDriver().get(baseURL);
+
+        Allure.step("Click on" + navbarMenu.toString());
+        getDriver().findElement(navbarMenu).click();
 
         Allure.step("Collect actualURL, actualTitle");
         final String actualURL = getDriver().getCurrentUrl();
