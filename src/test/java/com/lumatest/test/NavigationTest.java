e9e4d380f6a8ad4fa7f117aa01bbe0f7bcdb670b
Volhabo <barouskayavolha@gmail.com>
2024-06-08 16:24:59 +0200

    add groups

--- src/test/java/com/lumatest/test/NavigationTest.java
+++ src/test/java/com/lumatest/test/NavigationTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 public class NavigationTest extends BaseTest {
 
-    @Test(description = "TC-01 Open Base URL")
+    @Test(description = "TC-01 Open Base URL", groups = {"Smoke", "Regression"})
     @Story("Navigation")
     @Severity(SeverityLevel.BLOCKER)
     @Description("TC-01 Open Base URL")
@@ -33,6 +33,7 @@ public class NavigationTest extends BaseTest {
     }
 
     @Test(
+            groups = {"Smoke", "Regression"},
             description = "TC-02 Top Menu Navigation",
             dataProvider = "navigationData",
             dataProviderClass = TestData.class)
