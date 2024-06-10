cc0eb7674c2786740ba31acd0a6d9298809606e7
Volhabo <99760694+Volhabo@users.noreply.github.com>
2024-06-10 23:14:33 +0200

    add POM and testProduct(),update build.yml (#18)
    add POM and testProduct(), remove checkstyle from build.yml

---
+++ src/main/java/com/lumatest/model/BagsPage.java
@@ -0,0 +1,25 @@
+package com.lumatest.model;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BagsPage extends SideMenu {
+
+    private final String productNameXpath = "//img[@alt='";
+    private WebElement drivenBagpackImg;
+
+    protected BagsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Click + \'productName\' Img.")
+    public ProductPage clickDrivenBagpackImg(String productName) {
+        String imgXpath = productNameXpath + productName + "']";
+        getDriver().findElement(By.xpath(imgXpath)).click();
+
+        return new ProductPage(getDriver());
+    }
+}
---
+++ src/test/java/com/lumatest/test/ProductPageTest.java
@@ -0,0 +1,33 @@
+package com.lumatest.test;
+
+import com.lumatest.base.BaseTest;
+import com.lumatest.data.TestData;
+import com.lumatest.model.HomePage;
+import com.lumatest.model.ProductPage;
+import io.qameta.allure.Allure;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProductPageTest extends BaseTest {
+
+    @Test
+    public void testProduct() {
+        Allure.step("Open Base URL.");
+        getDriver().get(TestData.BASE_URL);
+
+        ProductPage productPage = new HomePage(getDriver())
+                .clickGearTopMenu()
+                .clickBagsSideMenu()
+                .clickDrivenBagpackImg(TestData.DRIVEN_BAGPACK_PRODUCT_NAME);
+
+        final String productName = productPage.getProductNameText();
+        final String breadCrumbsMenuText = productPage.getBreadCrumbsMenuText();
+        System.out.println(breadCrumbsMenuText);
+
+        Allure.step("Verify actual " + "'" + productName + "'" + " equals " + "'" + TestData.DRIVEN_BAGPACK_PRODUCT_NAME + "'");
+        Assert.assertEquals(productName, TestData.DRIVEN_BAGPACK_PRODUCT_NAME);
+
+        Allure.step("Verify actual " + "'" + breadCrumbsMenuText + "'" + " equals " + "'" + TestData.DRIVEN_BAGPACK_PRODUCT_PAGE_BREADCRUMPS_MENU_TEXT + "'");
+        Assert.assertEquals(breadCrumbsMenuText, TestData.DRIVEN_BAGPACK_PRODUCT_PAGE_BREADCRUMPS_MENU_TEXT);
+    }
+}
