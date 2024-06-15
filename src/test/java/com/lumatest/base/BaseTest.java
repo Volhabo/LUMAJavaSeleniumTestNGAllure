fb17b57007ce658d40091721bbb60e02bf92fdb9
Volhabo <barouskayavolha@gmail.com>
2024-06-15 17:23:51 +0200

    add chrome.xml

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -5,27 +5,33 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.*;
 
 public abstract class BaseTest {
     private WebDriver driver;
-//    private final String browser = "chromium";  //    private final String browser = "firefox";
+    private final ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
     @BeforeSuite
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
 //        WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.chromiumdriver().setup();
+//        WebDriverManager.chromiumdriver().setup();
     }
 
-   @Parameters("browser")
+    @Parameters("browser")
     @BeforeMethod
-    protected void setupDriver(@Optional("chrome") String browser, ITestResult result) {
+    protected void setupDriver(@Optional("chrome") String browser, ITestContext context, ITestResult result) {
         Reporter.log("------------------------------------------------------------------------", true);
-        Reporter.log("RUN "  + result.getMethod().getMethodName(), true);
+//        Reporter.log("RUN "  + result.getMethod().getMethodName(), true);
         this.driver = DriverUtils.createDriver(browser, this.driver);
+        this.threadLocalDriver.set(driver);
+
+        Reporter.log("Test Thread ID: " + Thread.currentThread().getId(), true);
+        Reporter.log("TEST SUIT: " + context.getCurrentXmlTest().getSuite().getName(), true);
+        Reporter.log("RUN " + result.getMethod().getMethodName(), true);
 
         if (getDriver() == null) {
             Reporter.log("Error: Unknown parameter 'browser' - " + browser + ". ", true);
@@ -33,22 +39,28 @@ public abstract class BaseTest {
             System.exit(1);
         }
 
-        Reporter.log("INFO: " + browser.toUpperCase() +  " driver created.", true);
+        Reporter.log("INFO: " + browser.toUpperCase() + " driver created.", true);
     }
 
     @Parameters("browser")
     @AfterMethod(alwaysRun = true)
     protected void tearDown(@Optional("chrome") String browser, ITestResult result) {
-        Reporter.log( result.getMethod().getMethodName() + ": " + result.getStatus(), true);
-        if (this.driver != null) {
-            getDriver().quit();
-            Reporter.log("INFO: " + browser.toUpperCase() +  " driver closed.", true);
+        Reporter.log(result.getMethod().getMethodName() + ": " + result.getStatus(), true);
 
-            this.driver = null;
+        if (getDriver() != null) {
+            getDriver().quit();
+            Reporter.log("INFO: " + browser.toUpperCase() + " driver closed.", true);
+
+            Reporter.log("After Test Thread ID: " + Thread.currentThread().getId(), true);
+            threadLocalDriver.remove();
+
+            driver = null;
+
         } else {
             Reporter.log("INFO: Driver is null.", true);
         }
     }
+
     public WebDriver getDriver() {
 
         return this.driver;
--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -58,14 +58,9 @@ public class DriverUtils {
     private static WebDriver createFirefoxDriver(WebDriver driver) {
         if (driver != null) {
             driver.quit();
-
-            return driver;
         }
-        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
-        // firefoxDriver.executeCdpCommand("Network.enable", Map.of());
-        // firefoxDriver.executeCdpCommand("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
 
-        return firefoxDriver;
+        return new FirefoxDriver(firefoxOptions);
     }
 
     private static WebDriver createChromiumDriver(WebDriver driver) {
